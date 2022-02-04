450dea635d98161d44a3239eba8b893fba5c6533
Fah2542 <Chatboolta2542@gmail.com>
2022-02-04 20:29:07 +0700

    first commit

---
+++ src/main/java/nvc/it/springapi/model/Review.java
@@ -0,0 +1,13 @@
+package nvc.it.springapi.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Review {
+    private Integer star;
+    private String comment;
+    
+}
